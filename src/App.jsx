3f196c522f9477c3d74abdbeaa910bfb32a2eafd
Anishkumar Patel <126298288+apatel2582@users.noreply.github.com>
2023-10-03 23:10:10 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Greeter from './Greeter';
+
+function App(){
+  return (
+    <>
+      <h1>Hello, REACT!</h1>
+      <Greeter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Greeter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Greeter(){
+    return (
+        <>
+            <h2>Hello, REACT! I am the Greeter component!</h2>
+        </>
+    );
+}
+
+export default Greeter;
